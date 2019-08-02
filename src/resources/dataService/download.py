c2ace8eb4f5fd0c10b74b325e44e27405b6217f6
cilegann <s571991@gmail.com>
2019-08-02 17:38:12 +0800

    add db

--- src/resources/dataService/download.py
+++ src/resources/dataService/download.py
@@ -1,7 +1,7 @@
-from flask import Flask,make_response
+from flask import Flask,make_response,abort
 from flask_restful import Api,Resource,reqparse
 from params import params
-from utils import tokenValidator
+from utils import tokenValidator,sql
 import glob
 from pathlib import Path
 import logging
@@ -17,36 +17,41 @@ class Download(Resource):
             parser = reqparse.RequestParser()
             parser.add_argument('fileUid', type=str,required=True)
             parser.add_argument('fileName',type=str)
-            parser.add_argument('tokenstr',type=str,required=True)
-            parser.add_argument('tokenint',type=int,required=True)
+            # parser.add_argument('tokenstr',type=str,required=True)
+            # parser.add_argument('tokenint',type=int,required=True)
             args = parser.parse_args()
             logging.debug(f"[Download] args: {args}")
             fileUid = args['fileUid']
             fileName=args['fileName']
-            # user=args['user']
-            tokenstr=args['tokenstr']
-            tokenint=args['tokenint']
+            # tokenstr=args['tokenstr']
+            # tokenint=args['tokenint']
 
-            #check token
-            if not tokenValidator(tokenstr,tokenint):
-                return {"status":"error","msg":"token error","data":{}},201
-            
-            paths=glob.glob(param.filepath+"/"+fileUid)
-            files=glob.glob(param.filepath+"/"+fileUid+".*")
-            logging.debug(f'[Download] paths {paths}')
-            logging.debug(f'[Download] files {files}')
-            if len(paths)==0 and len(files)==0:
-                from flask import abort
-
+            # #check token
+            # if not tokenValidator(tokenstr,tokenint):
+            #     return {"status":"error","msg":"token error","data":{}},201
+            try:
+                db=sql()
+                db.cursor.execute(f"select * from files where `fid`='{fileUid}';")
+                table=db.cursor.fetchall()
+            except Exception as e:
+                logging.error(e)
+                return {"status":"error","msg":str(e),"data":{}},201
+            finally:
+                db.conn.close()
+            table=[[tt for tt in t] for t in table]
+            logging.debug(f'[Download] {table}')
+            if len(table)==0:
                 logging.debug("[Download] file not found")
                 abort(404)
-            if len(files)==0:
-                filepath=paths[0]+'.zip'
-                shutil.make_archive(paths[0], 'zip', paths[0])
+            
+
+            table=table[0]
+            if table[1]=='cv':
+                filepath=table[2]+'.zip'
+                shutil.make_archive(table[2], 'zip', table[2])
                 filetype='.zip'
-                pass
             else:
-                filepath=files[0]
+                filepath=table[2]
                 filetype=filepath[filepath.rfind('.'):]
 
             with open(filepath,'rb') as file:
@@ -55,10 +60,13 @@ class Download(Resource):
             if filetype=='.zip':
                 os.remove(filepath)
             headers={}
+            if fileName==None:
+                fileName=fileUid
             headers['Content-Type']='application/octet-stream; charset=utf-8'
             headers['Content-Disposition'] = 'attachment; filename='+fileName+filetype
             return make_response(data,201,headers)
         except Exception as e:
+            logging.error(e)
             return {"status":"error","msg":str(e),"data":{}},201
         
         
