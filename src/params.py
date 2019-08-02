ed1297819b0faf28de1135e046a2aab2507bed4b
cilegann <s571991@gmail.com>
2019-08-02 17:36:51 +0800

    add db param

--- src/params.py
+++ src/params.py
@@ -4,3 +4,7 @@ class params():
         self.filepath='./files'
         self.modelpath='./models'
         self.dataFileType={'num':['.csv'],'cv':['.zip'],'nlp':['.tsv']}
+        self.dbhost='140.112.26.132'
+        self.dbuser='ican'
+        self.dbpwd='lab125a'
+        self.dbschema='incore'
