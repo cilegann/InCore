b65ea3a69669b59b41e437f23d11a11570ddecb6
cilegann <s571991@gmail.com>
2019-08-02 23:03:38 +0800

    move get file info from sql to util

--- src/resources/dataService/utils.py
+++ src/resources/dataService/utils.py
@@ -8,6 +8,20 @@ import uuid
 import shutil
 import logging
 import numpy as np
+from utils import sql
+
+def getFileInfo(fid):
+    try:
+        db=sql()
+        db.cursor.execute(f"select * from files where `fid`='{fid}'")
+        data=db.cursor.fetchall()
+        data=[[tt for tt in t] for t in data]
+        return {"status":"success","msg":"","data":data}
+    except Exception as e:
+        logging.error(f'[getFileInfo]{e}')
+        return {"status":"error","msg":str(e),"data":{}}
+    finally:
+        db.conn.close()
 
 class fileUidGenerator():
     def __init__(self):
@@ -44,7 +58,6 @@ class fileChecker():
                 if data[c].dtype!=np.float64 and data[c].dtype!=np.int64:
                     os.remove(self.filepath)
                     return {"status":"error","msg":"csv should only contain numerical value: (Col "+c+")","data":{}}
-                        
         except Exception as e:
             os.remove(self.filepath)
             return {"status":"error","msg":str(e),"data":{}}
@@ -53,16 +66,14 @@ class fileChecker():
     def cvZipChecker(self):
 
         folder=self.filepath[:self.filepath.rfind(".")]
-
-        # unzipping
         try:
             with zipfile.ZipFile(self.filepath, 'r') as zip_ref:
                 zip_ref.extractall(folder)
         except Exception as e:
             os.remove(self.filepath)
             return {"status":"error","msg":"unzip error","data":{}}
-
         os.remove(self.filepath)
+
         #check #.csv and location
         csvFiles=glob.glob((folder+"/*.csv"))
         if len(csvFiles)!=1 or len(csvFiles)==0:
@@ -83,128 +94,6 @@ class fileChecker():
         except Exception as e:
             return {"status":"error","msg":"error when parsing tsv using panda. "+str(e),"data":{}}
         return {"status":"success","msg":"",'data':{}}
-
-    # def numericalCsvChecker_old():
-    #     try:
-    #         data=pd.read_csv(self.filepath)
-
-    #         # check numerical value
-    #         for v in data.values:
-    #             for vv in v:
-    #                 try:
-    #                     tmp=float(vv)
-    #                 except Exception as e:
-    #                     os.remove(self.filepath)
-    #                     return {"status":"error","msg":"csv should only contain numerical value: "+str(vv),"data":{}}
-    #     except Exception as e:
-    #         os.remove(self.filepath)
-    #         return {"status":"error","msg":str(e),"data":{}}
-    #     return {"status":"success","msg":"","data":{}}
-
-    # def nlpTsvChecker_old():
-    #     try:
-    #         with open(self.filepath,newline='') as file:
-    #             rows =[r for r in csv.reader(file, delimiter='\t')]
-    #             cols=rows[0]
-
-    #         # check num of value col
-    #         if cols.count('value')!=1:
-    #             os.remove(self.filepath)
-    #             return {"status":"error","msg":"There should be (only) 1 col named value in csv file","data":{}}
-            
-    #         # check num of sentence col
-    #         sentenceCol=cols.remove('value')
-    #         if len(sentenceCol)==0:
-    #             os.remove(self.filepath)
-    #             return {"status":"error","msg":"There should be at least 1 col in csv that logs sentence","data":{}}
-
-    #         # check numerical value
-    #         data=pd.read_csv(self.filepath, sep='\t', header=0)
-    #         for v in data['value'].values:
-    #             try:
-    #                 tmp=float(v)
-    #             except Exception as e:
-    #                 os.remove(self.filepath)
-    #                 return {"status":"error","msg":"value col should only contain numerical value","data":{}}
-       
-    #     except Exception as e:
-    #         os.remove(self.filepath)
-    #         return {"status":"error","msg":str(e),"data":{}}
-    #     return {"status":"success","msg":"","data":{}}
-
-    # def cvZipChecker_old():
-    #     import zipfile
-    #     folder=self.filepath[:self.filepath.rfind(".")]
-    #     '''
-    #     @ try unzipping
-    #     '''
-    #     try:
-    #         with zipfile.ZipFile(self.filepath, 'r') as zip_ref:
-    #             zip_ref.extractall(folder)
-    #     except Exception as e:
-    #         os.remove(self.filepath)
-    #         return {"status":"error","msg":"unzip error","data":{}}
-        
-    #     '''
-    #     @ check #csv and location
-    #     '''
-    #     csvFiles=glob.glob(r(folder+"/*.csv"))
-    #     if len(csvFiles)!=1 or len(csvFiles)==0:
-    #         os.remove(self.filepath)
-    #         shutil.rmtree(folder)
-    #         return {"status":"error","msg":"zip should contains only 1 csv file","data":{}}
-    #     if csvFiles[0].count("/")!=3:
-    #         os.remove(self.filepath)
-    #         shutil.rmtree(folder)
-    #         return {"status":"error","msg":"csv file should be placed in the top path","data":{}}
-    #     csvFile=csvFiles[0]
-
-    #     '''
-    #     @ check csv content and all img file
-    #     '''
-    #     try:
-            
-    #         with open(csvFile,newline='') as file:
-    #             rows =[r for r in csv.reader(file)]
-    #             cols=rows[0]
-
-    #         # check num of value col
-    #         if cols.count('value')!=1:
-    #             os.remove(self.filepath)
-    #             shutil.rmtree(folder)
-    #             return {"status":"error","msg":"There should be (only) 1 col named value in csv file","data":{}}
-            
-    #         # check num of image col
-    #         imageCol=cols.remove('value')
-    #         if len(imageCol)==0:
-    #             os.remove(self.filepath)
-    #             shutil.rmtree(folder)
-    #             return {"status":"error","msg":"There should be at least 1 col in csv that logs filename","data":{}}
-
-    #         # check numerical value
-    #         data=pd.read_csv(csvFile)
-    #         for v in data['value'].values:
-    #             try:
-    #                 tmp=float(v)
-    #             except Exception as e:
-    #                 os.remove(self.filepath)
-    #                 shutil.rmtree(folder)
-    #                 return {"status":"error","msg":"value col should only contain numerical value","data":{}}
-            
-    #         # check image file existence
-    #         for c in imageCol:
-    #             for v in data[c].values:
-    #                 if not os.path.exists(os.path.join(folder,v)):
-    #                     os.remove(self.filepath)
-    #                     shutil.rmtree(folder)
-    #                     return {"status":"error","msg":"image file missing: "+v,"data":{}}
-            
-    #     except Exception as e:
-    #         os.remove(self.filepath)
-    #         shutil.rmtree(folder)
-    #         return {"status":"error","msg":str(e),"data":{}}
-
-    #     return {"status":"success","msg":"","data":{}}
 
 def dTypeConverter(dtype):
     if dtype==np.float64:
