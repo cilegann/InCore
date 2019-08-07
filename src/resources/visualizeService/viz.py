aa9976a20a87607363a6c588eced0c0f60ef0f2f
cilegann <s571991@gmail.com>
2019-08-07 16:28:34 +0800

    basic def

--- src/resources/visualizeService/viz.py
+++ src/resources/visualizeService/viz.py
@@ -1,5 +1,15 @@
 class paramSet():
-    pass
-class viz():
     def __init__(self):
+        self.xCol=[]
+        self.yCol=[]
+        self.type=None # data/model
+        self.params={}
+
+class viz():
+    def __init__(self,paramSet):
+        self.paramSet=paramSet
+        self.imgUid=None
+        
+
+    def do_viz(self):
         pass
