5168f661331e16f44939121a16db22349cff10c8
cilegann <s571991@gmail.com>
2019-08-08 20:20:06 +0800

    getdata ok

--- src/service/visualizeService/core/dataViz.py
+++ src/service/visualizeService/core/dataViz.py
@@ -2,6 +2,7 @@ from bokeh.embed import json_item,components
 from bokeh.plotting import figure
 from bokeh.models import CustomJS,SaveTool,Tool,CustomAction,HoverTool,CustomJSHover,ColumnDataSource
 import pandas as pd
+import numpy as np
 
 from service.dataService.utils import getFileInfo,getDf
 
@@ -10,7 +11,7 @@ class dataViz():
         self.fid=fid
         self.algoInfo=algoInfo
         self.dataCol=dataCol #{"x":"x_col","y":"y_col","value":"value_col"}
-        self.data=self.getData()
+        self.data=self.getData() #{"x":np.array,"y":np.array,"all":np.array,"value":np.array}
         self.figure=self.init_figure()
         self.imgId=""
         self.img=None
@@ -18,14 +19,28 @@ class dataViz():
         self.errorMsg=""
 
     def getData(self):
-        fileInfo=getFileInfo(self.fid)
-        if fileInfo['status']!='success':
-            raise Exception("[GetFileInfo] error:"+str(fileInfo['msg']))
-        fileInfo=fileInfo['data'][0]
+        try:
+            fileInfo=getFileInfo(self.fid)
+        except Exception as e:
+            raise Exception(f'[dataViz]{self.algoInfo["name"]}[getData]{e}')
+        fileInfo=fileInfo[0]
+
         filepath=fileInfo[2]
         if fileInfo[3]!=None:
             filepath=fileInfo[3]
-        
+        data={}
+        try:
+            rawdata=getDf(filepath,fileInfo[1])
+        except Exception as e:
+            raise Exception(f'[dataViz]{self.algoInfo["name"]}[getData]{e}')
+        data['all']=rawdata
+        if 'x' in self.dataCol:
+            data['x']=np.asarray(rawdata[self.dataCol['x']])
+        if 'y' in self.dataCol:
+            data['y']=np.asarray(rawdata[self.dataCol['y']])
+        if 'value' in self.dataCol:
+            data['value']=np.asarray(rawdata[self.dataCol['value']])
+        return data
         
 
     def init_figure(self):
