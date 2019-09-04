afec673c900942c3e4f0ff4ea5e71e196f4d76c3
cilegann <s571991@gmail.com>
2019-09-04 22:08:00 +0800

    add changeModelStatus

--- src/service/analyticService/utils.py
+++ src/service/analyticService/utils.py
@@ -1,8 +1,14 @@
 import uuid
 import glob
+from utils import sql
 class modelUidGenerator():
     def __init__(self):
         self.uid=str(uuid.uuid1())
         param=params()
         while len(glob.glob((param.modelpath+"/"+self.uid+"*")))!=0:
             self.uid=str(uuid.uuid1())
+
+def changeModelStatus(mid,status):
+    try:
+        db=sql()
+        db.cursor.execute(f"update models set `status` = '{status}' where `mid`='{mid}'")
