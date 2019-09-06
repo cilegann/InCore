b5be98f6e610529b02fce6e6623ad0f2b12aaed1
cilegann <s571991@gmail.com>
2019-09-06 17:52:04 +0800

    add checking

--- src/service/analyticService/controller/analytic.py
+++ src/service/analyticService/controller/analytic.py
@@ -7,6 +7,7 @@ import logging
 import json
 import importlib
 import traceback
+import os
 
 param=params()
 
@@ -21,9 +22,9 @@ class getAnalyticAlgoList(Resource):
             with open(param.analyticAlgoReg) as file:
                 reg=json.load(file)
             if args['dataType'] not in reg:
-                raise Exception(f"{args['dataType']} is not a valid dataType")
+                return {"status":"error","msg":f"{args['dataType']} is not a valid dataType","data":{}},400
             if args['projectType'] not in reg[args['dataType']]:
-                raise Exception(f"{args['projectType']} is not a valid projectType")
+                return {"status":"error","msg":f"{args['projectType']} is not a valid projectType","data":{}},400
             return {"status":"error","msg":"","data":reg[args['dataType']][args['projectType']]}
         except Exception as e:
             logging.error(f"[API_getAnalyticAlgoList] {traceback.format_exc()}")
@@ -32,16 +33,18 @@ class getAnalyticAlgoList(Resource):
 class getAnalyticAlgoParam(Resource):
     def get(self):
         try:
-            logging.info('[API_getAnalyticAlgoList]')
+            logging.info('[API_getAnalyticAlgoParam]')
             parser=reqparse.RequestParser()
             parser.add_argument('dataType',type=str,required=True)
             parser.add_argument('projectType',type=str,required=True)
             parser.add_argument('algoname',type=str,required=True)
             args = parser.parse_args()
+            if not os.path.exists(param.analyticServiceRoot+f"core/analyticAlgo/{args['dataType']}/{args['projectType']}/{args['algoname']}.json"):
+                return {"status":"error","msg":f"[API_getAnalyticAlgoParam] {args['dataType']}.{args['projectType']}.{args['algoname']} not exists","data":{}},400
             with open(param.analyticServiceRoot+f"core/analyticAlgo/{args['dataType']}/{args['projectType']}/{args['algoname']}.json") as file:
                 reg=json.load(file)
             return {"status":"error","msg":"","data":reg}
         except Exception as e:
             logging.error(f"[API_getAnalyticAlgoList] {traceback.format_exc()}")
-            return {"status":"error","msg":f"[API_getAnalyticAlgoList] {traceback.format_exc()}","data":{}},400
+            return {"status":"error","msg":f"[API_getAnalyticAlgoParam] {traceback.format_exc()}","data":{}},400
             
