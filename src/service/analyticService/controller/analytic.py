26915d56a612430f6b32f9f1482b2cf15c32909d
cilegann <s571991@gmail.com>
2019-09-06 17:45:23 +0800

    param checking

--- src/service/analyticService/controller/analytic.py
+++ src/service/analyticService/controller/analytic.py
@@ -20,10 +20,14 @@ class getAnalyticAlgoList(Resource):
             args = parser.parse_args()
             with open(param.analyticAlgoReg) as file:
                 reg=json.load(file)
+            if args['dataType'] not in reg:
+                raise Exception(f"{args['dataType']} is not a valid dataType")
+            if args['projectType'] not in reg[args['dataType']]:
+                raise Exception(f"{args['projectType']} is not a valid projectType")
             return {"status":"error","msg":"","data":reg[args['dataType']][args['projectType']]}
         except Exception as e:
             logging.error(f"[API_getAnalyticAlgoList] {traceback.format_exc()}")
-            return {"status":"error","msg":f"[API_getAnalyticAlgoList] {traceback.format_exc()}","data":{}}
+            return {"status":"error","msg":f"[API_getAnalyticAlgoList] {traceback.format_exc()}","data":{}},400
 
 class getAnalyticAlgoParam(Resource):
     def get(self):
@@ -39,5 +43,5 @@ class getAnalyticAlgoParam(Resource):
             return {"status":"error","msg":"","data":reg}
         except Exception as e:
             logging.error(f"[API_getAnalyticAlgoList] {traceback.format_exc()}")
-            return {"status":"error","msg":f"[API_getAnalyticAlgoList] {traceback.format_exc()}","data":{}}
+            return {"status":"error","msg":f"[API_getAnalyticAlgoList] {traceback.format_exc()}","data":{}},400
             
