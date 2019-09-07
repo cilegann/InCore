ebd9bab90ebc69819e077f79aca8d45b4febf5e9
cilegann <s571991@gmail.com>
2019-09-07 20:07:32 +0800

    change parameter. add label to report

--- src/service/analyticService/core/analyticAlgo/evaluateTools.py
+++ src/service/analyticService/core/analyticAlgo/evaluateTools.py
@@ -3,18 +3,18 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support as score
 from texttable import Texttable
 
-def crossEntropy(predictions, targets, epsilon=1e-12):
+def crossEntropy( targets,predictions, epsilon=1e-12):
     predictions = np.clip(predictions, epsilon, 1. - epsilon)
     N = predictions.shape[0]
     ce = -np.sum(targets*np.log(predictions+1e-9))/N
     return ce
 
-def classificationReport(predictions,targets,offset=3):
-    precision, recall, fscore, support = score(y_true, y_pred)
+def classificationReport(targets,predictions,label,offset=3):
+    precision, recall, fscore, support = score(targets, predictions,labels=label)
     t=Texttable()
     content=[['Label','Precision','Recall','FScore','Support']]
-    for i in range(len(precision)):
-        c=[str(i),precision[i],recall[i],fscore[i],support[i]]
+    for i,l in enumerate(label):
+        c=[l,precision[i],recall[i],fscore[i],support[i]]
         content.append(c)
     t.add_rows(content)
     txtSplit=t.draw().split('\n')
