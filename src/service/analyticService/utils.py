c9470aeca2a241e04f3d5ceb479745cd398f2296
cilegann <s571991@gmail.com>
2019-09-08 21:23:19 +0800

    get model info. filtering status

--- src/service/analyticService/utils.py
+++ src/service/analyticService/utils.py
@@ -1,6 +1,8 @@
 import uuid
 import glob
 from utils import sql
+from params import params
+
 class modelUidGenerator():
     def __init__(self):
         self.uid=str(uuid.uuid1())
@@ -8,7 +10,24 @@ class modelUidGenerator():
         while len(glob.glob((param.modelpath+"/"+self.uid+"*")))!=0:
             self.uid=str(uuid.uuid1())
 
+def getModelInfo(mid):
+    try:
+        db=sql()
+        db.cursor.execute(f"select * from files where `mid`='{mid}'")
+        data=db.cursor.fetchall()
+        data=[[tt for tt in t] for t in data]
+        if len(data)==0:
+            raise Exception(f'[getModelInfo] model {mid} not found')
+        return data
+    except Exception as e:
+        raise Exception(f'[getModelInfo]{e}')
+    finally:
+        db.conn.close()
+
 def changeModelStatus(mid,status):
+    statusList= ['train','fail','success']
+    if status not in statusList:
+        raise Exception("[changeModelStatus] invalid status")
     try:
         db=sql()
         db.cursor.execute(f"update models set `status` = '{status}' where `mid`='{mid}'")
