f159ed84e9ae6509da8be850443bc6e5e7daeb22
cilegann <s571991@gmail.com>
2019-09-09 13:10:32 +0800

    fix import. fix data-cate mapping

--- src/service/analyticService/core/analyticCore/classificationBase.py
+++ src/service/analyticService/core/analyticCore/classificationBase.py
@@ -2,7 +2,7 @@ from service.analyticService.core.analyticCore.analyticBase import analytic
 import numpy as np
 import pandas as pd
 from service.visualizeService.core.analyticVizAlgo.heatmap import heatmap
-from service.analyticService.core.analyticCore.evaluationTools import crossEntropy,classificationReport,confusionMatrix
+from service.analyticService.core.analyticCore.evaluateTools import crossEntropy,classificationReport
 from sklearn.metrics import confusion_matrix
 
 class classification(analytic):
@@ -14,9 +14,9 @@ class classification(analytic):
         for k, v in self.outputDict.items():
             self.txtRes += f"{v}:\n"
             self.txtRes += f"  Cross Entropy: {crossEntropy(self.outputData[k],self.result[k])}\n"
-            real=[self.c2d[v][str(i)] for i in  np.argmax(self.outputData[k],axis=1)]
-            predicted=[self.c2d[v][str(i)] for i in  np.argmax(self.result[k],axis=1)]
-            label=[k for k in self.d2c]
+            real=[str(self.c2d[v][str(i)]) for i in  np.argmax(self.outputData[k],axis=1)]
+            predicted=[str(self.c2d[v][str(i)]) for i in  np.argmax(self.result[k],axis=1)]
+            label=[k for k in self.d2c[v]]
             self.txtRes += f"  Report:\n{classificationReport(real,predicted,label=label)}"
         self.visualize()
         return {"text": self.txtRes, "fig": self.vizRes}
@@ -24,9 +24,9 @@ class classification(analytic):
     def projectVisualize(self):
         figs={}
         for k,v in self.outputDict.items():
-            real=[self.c2d[v][str(i)] for i in  np.argmax(self.outputData[k],axis=1)]
-            predicted=[self.c2d[v][str(i)] for i in  np.argmax(self.result[k],axis=1)]
-            label=[k for k in self.d2c]
+            real=[str(self.c2d[v][str(i)]) for i in  np.argmax(self.outputData[k],axis=1)]
+            predicted=[str(self.c2d[v][str(i)]) for i in  np.argmax(self.result[k],axis=1)]
+            label=[k for k in self.d2c[v]]
             cmx=confusion_matrix(real,predicted,labels=label)
             cmx=cmx.astype('float')/cmx.sum(axis=1)[:,np.newaxis]
             df=pd.DataFrame(cmx,columns=label)
