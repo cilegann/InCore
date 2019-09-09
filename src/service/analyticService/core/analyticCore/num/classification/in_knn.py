aabfa14763acbcb3caccdd5e01b464685a7d7993
cilegann <s571991@gmail.com>
2019-09-09 13:10:48 +0800

    new algo

--- src/service/analyticService/core/analyticCore/num/classification/in_knn.py
+++ src/service/analyticService/core/analyticCore/num/classification/in_knn.py
@@ -1,10 +1,10 @@
 from service.analyticService.core.analyticCore.classificationBase import classification
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
-class in_linearRegression(classification):
+class in_knn(classification):
     def trainAlgo(self):
-        self.model=KNeighborsClassifier(n_neighbors=self.param['n_nieghbors'],weights=self.param['weights'],algorithm=self.param['algorithm'],p=self.param['p'])
+        self.model=KNeighborsClassifier(n_neighbors=self.param['n_neighbors'],weights=self.param['weights'],algorithm=self.param['algorithm'],p=self.param['p'])
         y=np.argmax(self.outputData['Y'],axis=1)
-        self.model.fit(self.inputData['X'],self.outputData['Y'])
+        self.model.fit(self.inputData['X'],y)
     def predictAlgo(self):
         self.result['Y']=self.model.predict_proba(self.inputData['X'])
