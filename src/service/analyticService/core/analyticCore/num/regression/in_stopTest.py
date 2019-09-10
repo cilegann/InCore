df59c5ad43324e57ce7d5f2cf5aa43a810802a9e
cilegann <s571991@gmail.com>
2019-09-10 20:18:16 +0800

    algo to test stop

---
+++ src/service/analyticService/core/analyticCore/num/regression/in_stopTest.py
@@ -0,0 +1,10 @@
+from service.analyticService.core.analyticCore.regressionBase import regression
+from sklearn.linear_model import LinearRegression
+import time
+class in_linearRegression(regression):
+    def trainAlgo(self):
+        self.model=LinearRegression()
+        self.model.fit(self.inputData['X'],self.outputData['Y'])
+        time.sleep(600)
+    def predictAlgo(self):
+        self.result['Y']=self.model.predict(self.inputData['X'])
