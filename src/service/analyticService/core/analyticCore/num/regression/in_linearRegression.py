8738551954166340a709fea06304bf829c3688f9
cilegann <s571991@gmail.com>
2019-09-11 12:37:23 +0800

    add parameter

--- src/service/analyticService/core/analyticCore/num/regression/in_linearRegression.py
+++ src/service/analyticService/core/analyticCore/num/regression/in_linearRegression.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LinearRegression
 
 class in_linearRegression(regression):
     def trainAlgo(self):
-        self.model=LinearRegression()
+        self.model=LinearRegression(fit_intercept=self.param['fit_intercept'],normalize=self.param['normalize'])
         self.model.fit(self.inputData['X'],self.outputData['Y'])
     def predictAlgo(self):
         self.result['Y']=self.model.predict(self.inputData['X'])
