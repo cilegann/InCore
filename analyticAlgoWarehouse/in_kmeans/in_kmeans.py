60a927776a91d949294a11e8b6b978d7774889d2
cilegann <s571991@gmail.com>
2019-09-11 19:59:56 +0800

    new algo in warehouse

---
+++ analyticAlgoWarehouse/in_kmeans/in_kmeans.py
@@ -0,0 +1,9 @@
+from service.analyticService.core.analyticCore.clusteringBase import clustering
+from sklearn.cluster import KMeans
+
+class in_kmeans(clustering):
+    def trainAlgo(self):
+        self.model=KMeans(n_clusters=self.param['n_clusters'],max_iter=self.param['max_iter'],algorithm=self.param['algorithm'])
+        self.model.fit(self.inputData["X"])
+    def predictAlgo(self):
+        self.model.predict(self.inputData["X"])
