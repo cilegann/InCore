e09c9576a303194f0a9a50fa5a97f44a4822c9b5
cilegann <s571991@gmail.com>
2019-09-11 12:36:00 +0800

    add param

--- src/service/analyticService/core/analyticCore/num/regression/in_stopTest.py
+++ src/service/analyticService/core/analyticCore/num/regression/in_stopTest.py
@@ -3,8 +3,8 @@ from sklearn.linear_model import LinearRegression
 import time
 class in_stopTest(regression):
     def trainAlgo(self):
-        self.model=LinearRegression()
+        self.model=LinearRegression(fit_intercept=self.param['fit_intercept'],normalize=self.param['normalize'])
         self.model.fit(self.inputData['X'],self.outputData['Y'])
-        time.sleep(600)
+        time.sleep(self.param['secondToSleep'])
     def predictAlgo(self):
         self.result['Y']=self.model.predict(self.inputData['X'])
