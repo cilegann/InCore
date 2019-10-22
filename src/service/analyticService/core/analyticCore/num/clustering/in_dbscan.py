41c0b147c2ddfb21c7adc2f4645ee3fd31fdb5c2
cilegann <s571991@gmail.com>
2019-10-23 02:18:48 +0800

    new algo

---
+++ src/service/analyticService/core/analyticCore/num/clustering/in_dbscan.py
@@ -0,0 +1,39 @@
+
+from service.analyticService.core.analyticCore.clusteringBase import clustering
+from sklearn.cluster import DBSCAN
+
+
+
+class in_dbscan(clustering):
+    def trainAlgo(self):
+        if self.param['auto_eps']==1 and self.param['auto_min_samples']==1:
+            self.model=DBSCAN(
+                eps=self.param['eps'],
+                min_samples=self.param['min_samples'],
+                metric=self.param['metric'],
+                algorithm=self.param['algorithm'],
+                leaf_size=self.param['leaf_size']  
+            )
+        elif self.param['auto_eps']==1:
+            self.model=DBSCAN(
+                eps=self.param['eps'],
+                metric=self.param['metric'],
+                algorithm=self.param['algorithm'],
+                leaf_size=self.param['leaf_size']  
+            )
+        elif self.param['auto_min_samples']==1:
+            self.model=DBSCAN(
+                min_samples=self.param['min_samples'],
+                metric=self.param['metric'],
+                algorithm=self.param['algorithm'],
+                leaf_size=self.param['leaf_size']  
+            )
+        else:
+            self.model=DBSCAN(
+                metric=self.param['metric'],
+                algorithm=self.param['algorithm'],
+                leaf_size=self.param['leaf_size']  
+            )
+        self.model.fit(self.inputData["X"])
+    def predictAlgo(self):
+        self.result['cluster']=self.model.predict(self.inputData["X"])
