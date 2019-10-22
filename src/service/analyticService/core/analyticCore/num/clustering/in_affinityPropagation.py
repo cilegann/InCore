88b90fe6f7f08adfa2d04ff9a8b127a4baf53c3d
cilegann <s571991@gmail.com>
2019-10-22 23:24:32 +0800

    new algo

---
+++ src/service/analyticService/core/analyticCore/num/clustering/in_affinityPropagation.py
@@ -0,0 +1,17 @@
+
+from service.analyticService.core.analyticCore.clusteringBase import clustering
+from sklearn.cluster import AffinityPropagation
+
+
+
+class in_kmeans(clustering):
+    def trainAlgo(self):
+        self.model=AffinityPropagation(
+            dampling=self.param['dampling'],
+            max_iter=self.param['max_iter'],
+            convergence_iter=self.param['convergence_iter'],
+            affinity=self.param['affinity']
+            )
+        self.model.fit(self.inputData["X"])
+    def predictAlgo(self):
+        self.result['cluster']=self.model.predict(self.inputData["X"])
