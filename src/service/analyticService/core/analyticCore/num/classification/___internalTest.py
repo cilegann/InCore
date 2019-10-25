9a750ff0891064f1507fe01ea86592a2249258ac
inanalysis <admin@example.com>
2019-10-25 16:22:13 +0800

    test

--- src/service/analyticService/core/analyticCore/num/classification/___internalTest.py
+++ src/service/analyticService/core/analyticCore/num/classification/___internalTest.py
@@ -5,6 +5,20 @@ class ___internalTest(classification):
     def trainAlgo(self):
         self.model=KNeighborsClassifier(n_neighbors=self.param['n_neighbors'],weights=self.param['weights'],algorithm=self.param['algorithm'],p=self.param['p'])
         y=np.argmax(self.outputData['Y'],axis=1)
-        self.model.fit(self.inputData['X'],y)
+        x=[]
+        for i in range(len(self.inputData['X'])):
+            tmp=self.inputData['X'][i]
+            for j in range(len(self.inputData['CX'][i])):
+                tmp=np.append(tmp,self.inputData['CX'][i][j])
+            x.append(tmp)
+        x=np.asarray(x)
+        self.model.fit(x,y)
     def predictAlgo(self):
-        self.result['Y']=self.model.predict_proba(self.inputData['X'])
+        x=[]
+        for i in range(len(self.inputData['X'])):
+            tmp=self.inputData['X'][i]
+            for j in range(len(self.inputData['CX'][i])):
+                tmp=np.append(tmp,self.inputData['CX'][i][j])
+            x.append(tmp)
+        x=np.asarray(x)
+        self.result['Y']=self.model.predict_proba(x)
