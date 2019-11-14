46a2854d389a628112a76982f0d16b6fc5b2e347
cilegann <s571991@gmail.com>
2019-11-14 15:27:49 +0800

    CV test

---
+++ src/service/analyticService/core/analyticCore/utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+from PIL import Image
+import cv2
+def XYdataGenerator(fileList,yList,height,width,batchSize):
+    trainIndex=0
+    while(1):
+        lowerBound=trainIndex
+        if lowerBound+batchSize>len(fileList)-1:
+            upperBound=len(fileList)
+            trainIndex=0
+        else:
+            upperBound=lowerBound+batchSize
+            trainIndex=upperBound
+        todo_fileList=fileList[lowerBound:upperBound]
+        y=yList[lowerBound:upperBound]
+        x=[]
+        for f in todo_fileList:
+            img=np.asarray(Image.open(f).resize(width,height))
+            img=img.astype('float64')
+            img/=255.
+            x.append(img)
+        x=np.asarray(x)
+        yield x,y
+
+def XdataGenerator(fileList,height,width,batchSize):
+    trainIndex=0
+    while(1):
+        lowerBound=trainIndex
+        if lowerBound+batchSize>len(fileList)-1:
+            upperBound=len(fileList)
+            trainIndex=0
+        else:
+            upperBound=lowerBound+batchSize
+            trainIndex=upperBound
+        todo_fileList=fileList[lowerBound:upperBound]
+        x=[]
+        for f in todo_fileList:
+            img=np.asarray(Image.open(f).resize(width,height))
+            img=img.astype('float64')
+            img/=255.
+            x.append(img)
+        x=np.asarray(x)
+        yield x
