ac737c68a5f0bd0ec46f564a88fb311a77e491b1
inanalysis <admin@example.com>
2019-12-17 22:08:07 +0800

    change deadline

--- src/params.py
+++ src/params.py
@@ -39,4 +39,4 @@ class params():
         self.maintaining=False
         self.maintainMsg="為了提供穩定訓練環境，現在為系統維護時段，您仍可正常使用<br>但可能會出現瞬斷、模型訓練中止等情況。抱歉造成不便"
 
-        self.analyticModuleUploadDeadline='2019-12-18 00:00'
+        self.analyticModuleUploadDeadline='2019-12-18 01:00'
